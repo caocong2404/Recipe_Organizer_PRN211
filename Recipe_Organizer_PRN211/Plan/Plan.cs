6900f57874c3d5ad6795a812df2d65c2b10b24ca
PhiDz20 <phidoan5153@gmail.com>
2023-06-23 07:52:53 +0700

    view form plan

---
+++ Recipe_Organizer_PRN211/Plan/Plan.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Recipe_Organizer_PRN211.Plan
+{
+	public partial class Plan : Form
+	{
+		public Plan()
+		{
+			InitializeComponent();
+		}
+
+		private void ntnToDay_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button5_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btnSat_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btnFriday_Click(object sender, EventArgs e)
+		{
+		}
+
+		private void btnMonday_Click(object sender, EventArgs e)
+		{
+		}
+
+		private void btnThursday_Click(object sender, EventArgs e)
+		{
+		}
+
+		private void panel2_Paint(object sender, PaintEventArgs e)
+		{
+
+		}
+
+		private void panel4_Paint(object sender, PaintEventArgs e)
+		{
+
+		}
+
+		private void btnPreviours_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btnTuesday_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btnThusday_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
