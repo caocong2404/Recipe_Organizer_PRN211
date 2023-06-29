6a11c85a63dd4371735dd8ccab8cb5f6b243a4a0
PhiDz20 <phidoan5153@gmail.com>
2023-06-29 12:54:18 +0700

    fix program file

--- Recipe_Organizer_PRN211/Program.cs
+++ Recipe_Organizer_PRN211/Program.cs
@@ -13,7 +13,7 @@ namespace Recipe_Organizer_PRN211
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new /*Homepage*/PlanView());
+            Application.Run(new Homepage());
         }
     }
 }
