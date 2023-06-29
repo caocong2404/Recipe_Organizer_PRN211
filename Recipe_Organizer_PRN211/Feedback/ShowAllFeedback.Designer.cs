c1d339d3ed91cda5e97ff8d66a5884f42f67315c
Khanh Duy <vanduy71002@gmail.com>
2023-06-29 16:50:22 +0700

    feedback form

---
+++ Recipe_Organizer_PRN211/Feedback/ShowAllFeedback.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Recipe_Organizer_PRN211.Feedback
+{
+	partial class ShowAllFeedback
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			label1 = new Label();
+			textBox1 = new TextBox();
+			button1 = new Button();
+			SuspendLayout();
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Font = new Font("Montserrat ExtraBold", 13.7999992F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+			label1.ForeColor = Color.Red;
+			label1.Location = new Point(179, 27);
+			label1.Name = "label1";
+			label1.Size = new Size(136, 32);
+			label1.TabIndex = 0;
+			label1.Text = "Feedback";
+			// 
+			// textBox1
+			// 
+			textBox1.Location = new Point(12, 62);
+			textBox1.Multiline = true;
+			textBox1.Name = "textBox1";
+			textBox1.ScrollBars = ScrollBars.Vertical;
+			textBox1.Size = new Size(461, 324);
+			textBox1.TabIndex = 1;
+			// 
+			// button1
+			// 
+			button1.Font = new Font("Montserrat", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+			button1.Location = new Point(379, 409);
+			button1.Name = "button1";
+			button1.Size = new Size(94, 29);
+			button1.TabIndex = 2;
+			button1.Text = "Close";
+			button1.UseVisualStyleBackColor = true;
+			// 
+			// ShowAllFeedback
+			// 
+			AutoScaleDimensions = new SizeF(8F, 20F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(496, 450);
+			Controls.Add(button1);
+			Controls.Add(textBox1);
+			Controls.Add(label1);
+			Name = "ShowAllFeedback";
+			Text = "ShowAllFeedback";
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private Label label1;
+		private TextBox textBox1;
+		private Button button1;
+	}
+}
--- Recipe_Organizer_PRN211/Recipe/RecipeDetail.cs
+++ Recipe_Organizer_PRN211/Recipe/RecipeDetail.cs
@@ -1,4 +1,5 @@
-﻿using Services.Models;
+﻿using Recipe_Organizer_PRN211.Feedback;
+using Services.Models;
 using Services.Service;
 using Services.Services;
 using System;
@@ -13,92 +14,98 @@ using System.Windows.Forms;
 
 namespace Recipe_Organizer_PRN211.Recipe
 {
-    public partial class RecipeDetail : Form
-    {
-        private RecipeRepository _recipeRepository;
+	public partial class RecipeDetail : Form
+	{
+		private RecipeRepository _recipeRepository;
 
-        public RecipeDetail()
-        {
-            InitializeComponent();
-            _recipeRepository = new RecipeRepository();
-        }
-        protected override void OnLoad(EventArgs e)
-        {
-            base.OnLoad(e);
-            if (AppContext.RecipeId > 0)
-            {
-                // Get the recipe from the database
-                int recipeId = AppContext.RecipeId;
-                Services.Models.Recipe recipe = _recipeRepository.GetRecipe(recipeId);
+		public RecipeDetail()
+		{
+			InitializeComponent();
+			_recipeRepository = new RecipeRepository();
+		}
+		protected override void OnLoad(EventArgs e)
+		{
+			base.OnLoad(e);
+			if (AppContext.RecipeId > 0)
+			{
+				// Get the recipe from the database
+				int recipeId = AppContext.RecipeId;
+				Services.Models.Recipe recipe = _recipeRepository.GetRecipe(recipeId);
 
-                if (recipe != null)
-                {
-                    lbTitle.Text = recipe.Title;
+				if (recipe != null)
+				{
+					lbTitle.Text = recipe.Title;
 
-                    // Clear any existing controls in the table layout panel
-                    tlpIngredients.Controls.Clear();
+					// Clear any existing controls in the table layout panel
+					tlpIngredients.Controls.Clear();
 
-                    // Set the column and row styles to autosize
-                    tlpIngredients.ColumnStyles.Clear();
-                    tlpIngredients.RowStyles.Clear();
-                    tlpIngredients.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize));
+					// Set the column and row styles to autosize
+					tlpIngredients.ColumnStyles.Clear();
+					tlpIngredients.RowStyles.Clear();
+					tlpIngredients.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize));
 
-                    // Add a row for each ingredient
-                    string[] ingredients = recipe.Ingredient.Split(new[] { "\\n" }, StringSplitOptions.RemoveEmptyEntries);
-                    for (int i = 0; i < ingredients.Length; i++)
-                    {
-                        tlpIngredients.RowStyles.Add(new RowStyle(SizeType.AutoSize));
+					// Add a row for each ingredient
+					string[] ingredients = recipe.Ingredient.Split(new[] { "\\n" }, StringSplitOptions.RemoveEmptyEntries);
+					for (int i = 0; i < ingredients.Length; i++)
+					{
+						tlpIngredients.RowStyles.Add(new RowStyle(SizeType.AutoSize));
 
-                        // Create a Label control for the ingredient
-                        Label ingredientLabel = new Label();
-                        ingredientLabel.Text = ingredients[i];
-                        ingredientLabel.AutoSize = true;
+						// Create a Label control for the ingredient
+						Label ingredientLabel = new Label();
+						ingredientLabel.Text = ingredients[i];
+						ingredientLabel.AutoSize = true;
 
-                        // Add the Label control to the table layout panel
-                        tlpIngredients.Controls.Add(ingredientLabel, 0, i);
-                    }
+						// Add the Label control to the table layout panel
+						tlpIngredients.Controls.Add(ingredientLabel, 0, i);
+					}
 
-                    // Clear any existing controls in the table layout panel
-                    tlpDirection.Controls.Clear();
+					// Clear any existing controls in the table layout panel
+					tlpDirection.Controls.Clear();
 
-                    // Set the column and row styles to autosize
-                    tlpDirection.ColumnStyles.Clear();
-                    tlpDirection.RowStyles.Clear();
-                    tlpDirection.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize));
-                    tlpDirection.GrowStyle = TableLayoutPanelGrowStyle.AddRows;
+					// Set the column and row styles to autosize
+					tlpDirection.ColumnStyles.Clear();
+					tlpDirection.RowStyles.Clear();
+					tlpDirection.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize));
+					tlpDirection.GrowStyle = TableLayoutPanelGrowStyle.AddRows;
 
-                    // Add a row for each step in the description
-                    string[] steps = recipe.Description.Split(new[] { "\\n" }, StringSplitOptions.RemoveEmptyEntries);
-                    for (int i = 0; i < steps.Length; i++)
-                    {
-                        tlpDirection.RowStyles.Add(new RowStyle(SizeType.AutoSize));
+					// Add a row for each step in the description
+					string[] steps = recipe.Description.Split(new[] { "\\n" }, StringSplitOptions.RemoveEmptyEntries);
+					for (int i = 0; i < steps.Length; i++)
+					{
+						tlpDirection.RowStyles.Add(new RowStyle(SizeType.AutoSize));
 
-                        // Create a Label control for the step description
-                        Label stepLabel = new Label();
-                        stepLabel.Text = $"Step {i + 1}: {steps[i]}";
-                        stepLabel.AutoSize = true;
+						// Create a Label control for the step description
+						Label stepLabel = new Label();
+						stepLabel.Text = $"Step {i + 1}: {steps[i]}";
+						stepLabel.AutoSize = true;
 
-                        // Add the Label control to the table layout panel
-                        tlpDirection.Controls.Add(stepLabel, 0, i);
-                    }
+						// Add the Label control to the table layout panel
+						tlpDirection.Controls.Add(stepLabel, 0, i);
+					}
 
-                    lbDate.Text = "Date: " + recipe.Date.ToString();
-                }
-            }
-        }
+					lbDate.Text = "Date: " + recipe.Date.ToString();
+				}
+			}
+		}
 
-        private void btnBack_Click(object sender, EventArgs e)
-        {
-            this.Hide();
-            SearchRecipe recipeListForm = new SearchRecipe();
-            recipeListForm.ShowDialog();
-        }
+		private void btnBack_Click(object sender, EventArgs e)
+		{
+			this.Hide();
+			SearchRecipe recipeListForm = new SearchRecipe();
+			recipeListForm.ShowDialog();
+		}
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            Form searhRecipe = new SearchRecipe();
-            this.Hide();
-            searhRecipe.ShowDialog();
-        }
-    }
+		private void button1_Click(object sender, EventArgs e)
+		{
+			Form searhRecipe = new SearchRecipe();
+			this.Hide();
+			searhRecipe.ShowDialog();
+		}
+
+		private void btnAddFeedback_Click(object sender, EventArgs e)
+		{
+			FeedbackForm feedbackForm = new FeedbackForm();
+			feedbackForm.ShowDialog();
+		}
+	}
 }
